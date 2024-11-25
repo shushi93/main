059e4de885c93a96f8d8785fed1e60d55581d391
shushi93 <shashi0396a@gmail.com>
2024-11-25 13:35:54 +0800

    Update and rename bank.py to bank.py

--- CS50/CS50P/Problem Set 1/bank/bank.py
+++
@@ -1,9 +0,0 @@
-x = input("A customer comes in ").strip().lower()
-print("$", end = "")
-
-if (x.startswith("hello")):
-    print("0")
-elif (x.startswith("h")):
-    print("20")
-else:
-    print("100")
