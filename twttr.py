6fbc376e3a778887c52c6b1bb7c20ca12093dbdd
shushi93 <shashi0396a@gmail.com>
2024-11-26 18:09:54 +0800

    Update and rename twttr.py to CS50/CS50P/Problem Set 2/Just setting up my twttr/twttr.py

--- twttr.py
+++
@@ -1,7 +0,0 @@
-s = input("Hello ")
-for c in s:
-    #print(c)
-    if (c.lower() != "a" and c.lower()  != "e" and c.lower() != "i" and c.lower() != "o" and c.lower() != "u"):
-        print(c, end = "")
-    else:
-        continue
